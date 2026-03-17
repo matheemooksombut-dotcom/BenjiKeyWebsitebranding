80e8864ce3814c77325d705ad13257d9cd9bd50c
matheemooksombut-dotcom <matheemooksombut@gmail.com>
2026-03-17 13:02:42 +0700

    Updated

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,7 +7,32 @@ function Footer() {
 
   return (
     <>
-        
+        <div className="footer-wrapper">
+            <div className="footer-content">
+              <center><h2>TESTIMONIALS</h2></center>
+            </div>
+
+            <div className='footer-flex-content'>
+              <div className="foot-insdie">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam tenetur nobis saepe quae reiciendis explicabo quibusdam non earum necessitatibus porro.</p>
+                <br />
+                <b><p>BETTY DOWNEY</p></b>
+              </div>
+
+              <div className="foot-insdie">
+                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam tenetur nobis saepe quae reiciendis explicabo quibusdam non earum necessitatibus porro.</p>
+                 <br />
+                 <b><p>JAMES T. O'REILLY</p></b>
+              </div>
+
+              <div className="foot-insdie">
+                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam tenetur nobis saepe quae reiciendis explicabo quibusdam non earum necessitatibus porro.</p>
+                 <br />
+                 <b><p>TOM & MANDY LEVIS</p></b>
+              </div>
+            </div>
+
+        </div>
 
               
     </>
