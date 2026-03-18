2bfc62b38c4d7b69251e3a5f80ded50a728ae185
matheemooksombut-dotcom <matheemooksombut@gmail.com>
2026-03-18 21:02:02 +0700

    Updated

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,7 @@
 
 import { useState } from 'react'
-
-
 function Footer() {
+
   const [count, setCount] = useState(0)
 
   return (
@@ -11,15 +10,17 @@ function Footer() {
 
         <div className="incontent-footer">
               <div className='main-block-contentfooter'>
+                
+                 <iframe className='map-box'
+                    src="https://www.google.com/maps?q=13.736717,100.523186&z=15&output=embed"
+                    width="40%"
+                    height="300"
+                    style={{ border: 0 }}
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                  ></iframe>
+
                 <div className="block-footer-list">
-
-                  <div className='Inconcept-footer'>
-                    <div className='Inconcept-img'>
-                      <img src="./public/LogoTemplate.jpg" alt="" />
-
-                    </div>
-                    <span>Benji's Lock & Key</span>
-                  </div>
 
                   <ul className='list-footer'>
                     <li>Residential</li>
@@ -48,14 +49,14 @@ function Footer() {
                     <li>Email</li>
                     <br />
                     <li>Info@mysite.com</li>
-                  </ul>
-
-
-                  <ul className='list-footer'>
+                    <br />
+                    <div className='underline'></div>
+                    <br />
                     <li>Phone</li>
                     <br />
                     <li>123-456-7890</li>
                   </ul>
+
                 </div>
                 
               </div>
